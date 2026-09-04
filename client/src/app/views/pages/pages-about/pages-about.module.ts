09643f36107cfcf7406b16cefdffb809f7419f6f
Jaime <102960711+Dante690@users.noreply.github.com>
2026-09-04 13:21:50 +0200

    Refactor PagesAbout module and content to reflect project rebranding from CIViC to ERA: update module imports, modify page titles and descriptions, and remove outdated funding information.

--- client/src/app/views/pages/pages-about/pages-about.module.ts
+++ client/src/app/views/pages/pages-about/pages-about.module.ts
@@ -5,21 +5,15 @@ import { NzPageHeaderModule } from 'ng-zorro-antd/page-header'
 import { NzGridModule } from 'ng-zorro-antd/grid'
 import { NzSpaceModule } from 'ng-zorro-antd/space'
 import { NzCardModule } from 'ng-zorro-antd/card'
-import { NzTableModule } from 'ng-zorro-antd/table'
-import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions'
-import { CvcPreferredCitationsComponent } from '@app/components/shared/preferred-citations/preferred-citations.component'
 
 @NgModule({
   declarations: [PagesAboutPage],
   imports: [
     CommonModule,
     NzPageHeaderModule,
-    NzDescriptionsModule,
     NzGridModule,
     NzSpaceModule,
     NzCardModule,
-    NzTableModule,
-    CvcPreferredCitationsComponent,
   ],
 })
 export class PagesAboutModule {}
